a4510e8b7fbfac8c3d5b0e84d0d1ec7d74f7873a
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2025-02-05 13:01:09 -0600

    start of project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Header Component</h1>
+    </header>
+  );
+};
+
+export default Header;
